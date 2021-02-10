a8b067fd10f95e86b0eb3c8bfa9d60a855b2aeae
MonaK13 <44610964+monak13@users.noreply.github.com>
2021-02-10 14:37:46 +0100

    Changes: - basic fonts & styles implemented - changeable button element implemented

--- Prototype/angular-app/src/app/app.module.ts
+++ Prototype/angular-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './modular-elements/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
