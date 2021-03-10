c21f95d73994759d3f3180c9274d92d87f1b2c6a
MonaK13 <44610964+monak13@users.noreply.github.com>
2021-03-10 18:45:34 +0100

    Changes: - stepper updated to new design (unfinished) - building kit updated to drag-and-drop-list

--- Prototype/angular-app/src/app/pages/building-kit-page/building-kit-page.component.ts
+++ Prototype/angular-app/src/app/pages/building-kit-page/building-kit-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-building-kit-page',
@@ -10,6 +11,77 @@ export class BuildingKitPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sidebar;
+  main;
+
+  @ViewChildren(CdkDrag) draggables: QueryList<CdkDrag>;
+
+  ngAfterViewInit() {
+    const arr = this.draggables.toArray();
+    this.sidebar = [arr[0], arr[1], arr[2]];
+    this.main = [];
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    const nodeToMove = event.item.element.nativeElement;
+    const { previousContainer, container, previousIndex, currentIndex } = event;
+
+    console.log(this.sidebar, this.main);
+
+    if (previousContainer === container) {
+      moveItemInArray(container.data, previousIndex, currentIndex);
+
+      moveWithinContainer(
+        container.element.nativeElement,
+        previousIndex,
+        currentIndex
+      );
+    } else {
+      transferArrayItem(
+        previousContainer.data,
+        container.data,
+        previousIndex,
+        currentIndex
+      );
+      transferNodeToContainer(
+        nodeToMove,
+        container.element.nativeElement,
+        currentIndex
+      );
+
+      Promise.resolve().then(() => {
+        previousContainer.removeItem(event.item);
+        event.item.dropContainer = container;
+        event.item._dragRef._withDropContainer(container._dropListRef);
+        container.addItem(event.item);
+      });
+    }
+  }
+}
+
+function moveWithinContainer(container, fromIndex, toIndex) {
+  if (fromIndex === toIndex) {
+    return;
+  }
+
+  const nodeToMove = container.children[fromIndex];
+  const targetNode = container.children[toIndex];
+
+  if (fromIndex < toIndex) {
+    targetNode.parentNode.insertBefore(nodeToMove, targetNode.nextSibling);
+  } else {
+    targetNode.parentNode.insertBefore(nodeToMove, targetNode);
+  }
+}
+
+function transferNodeToContainer(node, container, toIndex) {
+  if (toIndex === container.children.length) {
+    container.appendChild(node);
+  } else {
+    const targetItem = container.children[toIndex];
+    targetItem.parentNode.insertBefore(node, targetItem);
   }
 
 }
